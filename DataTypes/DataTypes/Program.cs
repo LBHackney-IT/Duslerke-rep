577adc30e705103150374999c9e4075eddb16e54
Liudvikas Taluntis <liudvikast@gmail.com>
2018-11-04 21:23:13 +0000

    added a Random Debtors generator. also added code in Main() to create 'n' amount of debtors and store them into array.

--- DataTypes/DataTypes/Program.cs
+++ DataTypes/DataTypes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DataTypes;
 
 namespace DataTypes
 {
@@ -6,9 +7,9 @@ namespace DataTypes
     struct PersonWithDebt //Let's assume there are loads of people who owe me money
     {
         public int personID; //need to have a way to track them down
-        public Int16 debt; // I don't really lend sums larger than 65k pounds.
+        public float debt; // I don't really lend sums larger than 65k pounds.
 
-        public PersonWithDebt(int apersonID, int adebt) //constructor, so I wouldn't have to bother writing separate lines to initialise
+        public PersonWithDebt(int apersonID, float adebt) //constructor, so I wouldn't have to bother writing separate lines to initialise
         {
             this.personID = apersonID;
             this.debt = adebt;
@@ -21,10 +22,27 @@ namespace DataTypes
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
+            int numbOfDebtors = 3;
+            PersonWithDebt[] debtorsList = new PersonWithDebt[numbOfDebtors];
+
+            for (int i=0; i<numbOfDebtors; i++)
+            {
+                GenerateDebtors();
+            }
 
 
+            Console.WriteLine("[{0}]", string.Join(", ", debtorsList));
             Console.ReadKey();
         }
+
+        static PersonWithDebt GenerateDebtors()
+        {
+            Random random = new Random();
+            int randomID = random.Next(2200000, 2990000);
+            float randomDebt = random.Next(10000, 800000) / 100f;
+
+            return new PersonWithDebt(randomID, randomDebt);
+        }
+
     }
 }
