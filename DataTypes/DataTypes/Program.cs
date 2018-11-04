848ffc70844d574e270741149df57a172bf94531
Liudvikas Taluntis <liudvikast@gmail.com>
2018-11-04 23:18:57 +0000

    Added a pass by reference demonstration, reorded some comments to match the added code.

--- DataTypes/DataTypes/Program.cs
+++ DataTypes/DataTypes/Program.cs
@@ -16,6 +16,17 @@ namespace DataTypes
         }
     }
 
+    class PersonWithDebtClass // I guess, I'll add the class of the same thing for demonstration purposes. Everything apart the class instead of struct, is the same.
+    {
+        public int personID;
+        public float debt;
+
+        public PersonWithDebtClass(int apersonID, float adebt)
+        {
+            this.personID = apersonID;
+            this.debt = adebt;
+        }
+    }
 
     class Program
     {
@@ -29,11 +40,20 @@ namespace DataTypes
                 debtorsList[i] = GenerateDebtors(); //put the debtor into debtor array
             }
 
+            Console.WriteLine("Calling update method on value type:");
             //So let's assume my 7th debtor repayed 100 pounds and 60 pence, and I want to update it.
             Console.WriteLine("ID: {0} ; Debt: {1}", debtorsList[7].personID, debtorsList[7].debt); //Want to see his debt prior to payment on screen
             ChangeDebt(debtorsList[7], 100.60f); //I update his debt
             Console.WriteLine("ID: {0} ; Debt: {1}", debtorsList[7].personID, debtorsList[7].debt); //Should show his updated debt..Oh no! What happened?! More info on ChangeDebt() method comments.
             //Why not use class and create Objects? Well I read that there is somekind of performance difference because you 1) you don't have to allocate memory on heap. 2) you already have your entire unit of data on stack, which is faster..but not really sure is the performance worth it. To be honest.. I would want to get the answer from you on this question. 
+
+            PersonWithDebtClass debtorObj = new PersonWithDebtClass(2601872, 5400.37f); //create and instantiate my debtor object
+
+            Console.WriteLine("Calling update method on reference type:");
+            //So, same scenario - debtor wants to pay some money back
+            Console.WriteLine("ID: {0} ; Debt: {1}", debtorObj.personID, debtorObj.debt); //Want to see his debt prior to payment on screen
+            ChangeDebtClass(debtorObj, 1870.60f); //I update his debt
+            Console.WriteLine("ID: {0} ; Debt: {1}", debtorObj.personID, debtorObj.debt); // Debt updated.
 
 
             Console.ReadKey(); //Just pause the program, so I could read text 
@@ -50,11 +70,17 @@ namespace DataTypes
 
         static void ChangeDebt(PersonWithDebt person, float payment) //this won't work because structs are value types. Meaning rather than inputing a specific PersonWithDebt, I input it's copy because pass by copy applies.
         {
-            person.debt -= payment; //So I'm changing the debt attribute of the copy of the actual debtor, not the debtor itself. On the other hand, if instead of struct I wrote class. I would have had a class after which I would have made objects. And in my debtorsList array, I would have had references (addresses) to a location in heap memory, where those objects reside...instead of having actual objects sitting on the stack memory.
-        } //So continuing on that hypothetical scenario, I would have passed a reference of Debtor object into a ChangeDebt methods "person" parameter. So following that there would be a "person" variable created on the stack, that would point to the Debtor object. This way this method would access a real debtor rather than it's copy.
+            person.debt -= payment; //So I'm changing the debt attribute of the copy of the debtor, not the actual debtor itself. 
+        } 
 
         //Well the debt can be updated, but it would have to be a more direct way: debtList[7].debt -= payment;
         // So If I understand this correctly, you use struts mostly for the data that doesn't change. Like having students in database, where attributes are: name, age, gender, degree ... or something among those lines.
+
+        static void ChangeDebtClass(PersonWithDebtClass person, float payment) //On the other hand, if instead of struct I wrote class. I would have had a class after which I would have made objects.And in my debtorsList array, I would have had references (addresses) to a location in heap memory, where those objects reside...instead of having actual objects sitting on the stack memory.
+        {
+            person.debt -= payment; //So continuing on that hypothetical scenario, I would have passed a reference of Debtor object into a ChangeDebt methods "person" parameter. So following that there would be a "person" variable created on the stack, that would point to the Debtor object. This way this method would access a real debtor rather than it's copy.
+        }//I wrote this text before I thought of adding a class as well, just didn't want to change the text.
+
 
 
         // Of course I went all fancy with structs on this task, but to cover the rest:
