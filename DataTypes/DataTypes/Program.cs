6176eda2357f8267d8fd5aefed416047e0618ae4
Liudvikas Taluntis <liudvikast@gmail.com>
2018-11-04 20:19:27 +0000

    Initial commit, played around with git commands. Also wrote my struct for my new type of data.

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DataTypes
+{
+
+    struct PersonWithDebt //Let's assume there are loads of people who owe me money
+    {
+        public int personID; //need to have a way to track them down
+        public Int16 debt; // I don't really lend sums larger than 65k pounds.
+
+        public PersonWithDebt(int apersonID, int adebt) //constructor, so I wouldn't have to bother writing separate lines to initialise
+        {
+            this.personID = apersonID;
+            this.debt = adebt;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
