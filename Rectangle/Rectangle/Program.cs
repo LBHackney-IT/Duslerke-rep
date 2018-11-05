afd20082e0c5e93b6b798ac249c5395f595e94ef
Liudvikas Taluntis <liudvikast@gmail.com>
2018-11-05 01:12:21 +0000

    Wrote code for getArea() and Display() methods. Fixed the bug, where after inputing a string for the first try, awidth would get assigned nonsense value. Finished up the code in Main() method.

--- Rectangle/Rectangle/Program.cs
+++ Rectangle/Rectangle/Program.cs
@@ -6,10 +6,11 @@ namespace Rectangle
     {
         static void Main(string[] args) //there's a better way of doing this entire task, but it conflicts with specs given.
         {
-            Console.WriteLine("Hello World!");
             RectangleApplication temporary = new RectangleApplication();
             temporary.AcceptDetails();
-
+            temporary.getArea();
+            temporary.Display();
+            Console.ReadKey();
         }
     }
 
@@ -20,7 +21,6 @@ namespace Rectangle
         private double area;
         private double awidth;
         private double aheight;
-        private bool errFlag = false;
 
         public void AcceptDetails() 
         {
@@ -28,62 +28,66 @@ namespace Rectangle
             Console.Write("Enter rectangle width: ");
             while (true)
             {
-                this.errFlag = false;
                 try
                 {
                     awidth = Convert.ToDouble(Console.ReadLine());
+
+                    if (awidth >= 0)
+                    {
+                        this.width = awidth;
+                        break;
+                    }
+                    else if (awidth < 0)
+                    {
+                        Console.Write("Width can't be negative! Try again: ");
+                    }
+
+
                 } catch(FormatException e)
                 {
                     Console.Write(e.Message);
                     Console.Write(" Try again: ");
-                    this.errFlag = true;
                 }
 
-                if (awidth >= 0)
-                {
-                    this.width = awidth;
-                    break;
-                }
-                else if (awidth < 0 && !this.errFlag)
-                {
-                    Console.Write("Width can't be negative! Try again: ");
-                }
+
             }
 
             Console.Write("Enter rectangle height: ");
             while (true)
             {
-                this.errFlag = false;
                 try
                 {
                     aheight = Convert.ToDouble(Console.ReadLine());
+
+                    if (aheight >= 0)
+                    {
+                        this.height = aheight;
+                        break;
+                    }
+                    else if (aheight < 0)
+                    {
+                        Console.Write("Heigth can't be negative! Try again: ");
+                    }
+
                 }
                 catch (FormatException e)
                 {
                     Console.Write(e.Message);
                     Console.Write(" Try again: ");
-                    this.errFlag = true;
                 }
 
-                if (aheight >= 0)
-                {
-                    this.height = aheight;
-                    break;
-                } else if (aheight < 0 && !this.errFlag)
-                {
-                    Console.Write("Heigth can't be negative! Try again: ");
-                }
+
             }
         }
 
         public void getArea()
         {
-
+            this.area = this.awidth * this.aheight;
         }
 
         public void Display()
         {
-
+            Console.WriteLine("Rectangle area is: {0}", this.area);
         }
     }
 }
