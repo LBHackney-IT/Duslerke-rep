9cdbb55b911a83734dd806f4b5a3cb49c6854560
Liudvikas Taluntis <liudvikast@gmail.com>
2018-11-05 02:19:14 +0000

    DataTypes: Added pointers demonstration, and a bit of text at the bottom.

--- DataTypes/DataTypes/Program.cs
+++ DataTypes/DataTypes/Program.cs
@@ -30,7 +30,7 @@ namespace DataTypes
 
     class Program
     {
-        static void Main(string[] args)
+        static unsafe void Main(string[] args)
         {
             int numbOfDebtors = 10;
             PersonWithDebt[] debtorsList = new PersonWithDebt[numbOfDebtors]; //want initialise empty array of the PersonWithDebt variable type.
@@ -55,6 +55,12 @@ namespace DataTypes
             ChangeDebtClass(debtorObj, 1870.60f); //I update his debt
             Console.WriteLine("ID: {0} ; Debt: {1}", debtorObj.personID, debtorObj.debt); // Debt updated.
 
+
+            Console.WriteLine("\nSome pointers code");
+
+            int *smth = & numbOfDebtors; // good, so now I have a pointer to numbOfDebtors integer.
+            Console.WriteLine("Adress of numbOfDebtors: {0}", (int)smth); //has to be star'less.
+            Console.WriteLine(*smth);
 
             Console.ReadKey(); //Just pause the program, so I could read text 
         }
@@ -87,7 +93,9 @@ namespace DataTypes
 
         //Value types: So that's mostly primitives like int,float,boolean and etc. that get stored on Stack memory, which is smaller than heap, but quicker access. New data types created by struct also go here. These values can only be changed by reasigning them. Also when they are being worked with, in actuality, it's their value copies that are being worked on.
         //Reference types: These variables hold only the reference to an actual Object or String that is sitting somewhere on the heap memory. Since the reference is something like 4bytes, it doesn't eat up much memory on stack. When you pass this variable, you're only passing the address of memory cell, where and actual Object or String is defined. 
-        //Pointer types. From what I've read pointers and references are almost the same thing in C#. So I'm not really sure, what answer you're expecting. But I did read that when you create an Object instance, apart from it's attributes and methods, you get a: 1) Sync Block Index pointer - (no idea what this is, yet) 2) Type Object pointer, which allows us to know about the type of actual object.
-        //well, if you create a link list or something, there would be a struct with a value and pointer attribute. The pointer would give location of next element in the list. You could also have pointer that's reading this linked list - the pointer would always point to currently selected cell. If that's not the answer, then I don't know what is.
+        //Pointer types. From what I've read pointers and references are almost the same thing in C#.  But I did read that when you create an Object instance, apart from it's attributes and methods, you get a: 1) Sync Block Index pointer - (no idea what this is, yet) 2) Type Object pointer, which allows us to know about the type of actual object.
+        //If you create a link list or something, there would be a struct with a value and pointer attribute. The pointer would give location of next element in the list. You could also have pointer that's reading this linked list - the pointer would always point to currently selected cell. 
+
+        //Did some more research.  You can have a variable that's designed to store memory addresses.. it's marked by a star, you can also get addresses... I'll just write the code in Main() - it's self explanatory.
     }
 }
